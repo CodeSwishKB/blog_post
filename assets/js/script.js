ead6de2cf0b77139bc3815d4630902520bde6e6b
Kyle Barco <kylebarco04@gmail.com>
2023-08-03 11:08:09 +0800

    entry 04

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,17 +32,28 @@ document.addEventListener("DOMContentLoaded", function () {
       let timeAgo
 
       if (timeDifferenceInSeconds < 60) {
-        timeAgo = timeDifferenceInSeconds + " seconds ago"
+        timeAgo =
+          timeDifferenceInSeconds === 1
+            ? "second ago"
+            : timeDifferenceInSeconds + "1 seconds ago"
       } else if (timeDifferenceInSeconds < 3600) {
-        timeAgo = Math.floor(timeDifferenceInSeconds / 60) + " minutes ago"
+        const mins = Math.floor(timeDifferenceInSeconds / 60)
+        timeAgo = mins === 1 ? "1 minute ago" : mins + " minutes ago"
       } else if (timeDifferenceInSeconds < 86400) {
-        timeAgo = Math.floor(timeDifferenceInSeconds / 3600) + " hours ago"
+        const hours = Math.floor(timeDifferenceInSeconds / 3600)
+        timeAgo = hours === 1 ? "1 hour ago" : hours + " hours ago"
+      } else if (timeDifferenceInSeconds < 604800) {
+        const days = Math.floor(timeDifferenceInSeconds / 86400)
+        timeAgo = days === 1 ? "1 day ago" : days + " days ago"
       } else if (timeDifferenceInSeconds < 2592000) {
-        timeAgo = Math.floor(timeDifferenceInSeconds / 86400) + " days ago"
+        const weeks = Math.floor(timeDifferenceInSeconds / 604800)
+        timeAgo = weeks === 1 ? "1 week ago" : weeks + " weeks ago"
       } else if (timeDifferenceInSeconds < 31536000) {
-        timeAgo = Math.floor(timeDifferenceInSeconds / 2592000) + " months ago"
+        const months = Math.floor(timeDifferenceInSeconds / 2592000)
+        timeAgo = months === 1 ? "1 month ago" : months + " months ago"
       } else {
-        timeAgo = Math.floor(timeDifferenceInSeconds / 31536000) + " years ago"
+        const years = Math.floor(timeDifferenceInSeconds / 31536000)
+        timeAgo = years === 1 ? "1 year ago" : years + " years ago"
       }
 
       element.textContent = timeAgo
